c5b07aff93b5783aba7cdad6712107b30c4a919c
31932 <3193286091@qq.com>
2020-07-14 22:58:58 +0800

    第5章第1节

---
+++ chapter5-1/src/main/java/com/example/chapter51/Service/BookService.java
@@ -0,0 +1,30 @@
+package com.example.chapter51.Service;
+
+
+import com.example.chapter51.Bean.Book;
+import com.example.chapter51.Dao.BookDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    @Autowired
+    BookDao bookDao;
+    public int addBook(Book book){
+        return bookDao.addBook(book);
+    }
+    public int updateBook(Book book){
+        return bookDao.updateBook(book);
+    }
+    public int deleteBookById(Integer id){
+        return bookDao.deleteBookById(id);
+    }
+    public Book getBookById(Integer id){
+        return bookDao.getBookById(id);
+    }
+    public List<Book> getAllBooks(){
+        return bookDao.getAllBooks();
+    }
+}
---
+++ chapter5-1/src/test/java/com/example/chapter51/Chapter51ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chapter51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter51ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
