6df703d85fb5355169431800fabd8584cdb9bb00
31932 <3193286091@qq.com>
2020-07-15 16:25:31 +0800

    5-4-1 jdbcTemplate多数据源

---
+++ chapter5-4-1/src/main/java/com/example/chapter541/Chapter541Application.java
@@ -0,0 +1,13 @@
+package com.example.chapter541;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter541Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter541Application.class, args);
+	}
+
+}
---
+++ chapter5-4-1/src/test/java/com/example/chapter541/Chapter541ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chapter541;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter541ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
