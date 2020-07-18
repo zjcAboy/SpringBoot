cbde05e0f8b5272ed3bdd769abf0208a7cfef933
31932 <3193286091@qq.com>
2020-07-18 18:15:41 +0800

    5-4-2 MyBatis多数据源

---
+++ chapter5-4-2/src/main/java/com/example/chapter542/Chapter542Application.java
@@ -0,0 +1,13 @@
+package com.example.chapter542;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter542Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter542Application.class, args);
+    }
+
+}
