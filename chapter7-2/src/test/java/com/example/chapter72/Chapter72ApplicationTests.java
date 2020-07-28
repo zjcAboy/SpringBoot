6ac6750a8dc4f8d919dfd8b836a0e5f98958d5f6
31932 <3193286091@qq.com>
2020-07-28 16:58:18 +0800

    7-2 JPA实现REST

---
+++ chapter7-2/src/test/java/com/example/chapter72/Chapter72ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chapter72;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter72ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
