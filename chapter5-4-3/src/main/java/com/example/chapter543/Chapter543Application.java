13e3f259a2a45d5594466bb4d839debb988a326e
31932 <3193286091@qq.com>
2020-07-29 18:29:24 +0800

    8-1 devtools简介 8-2 devtools实战

---
+++ chapter5-4-3/src/main/java/com/example/chapter543/Chapter543Application.java
@@ -0,0 +1,13 @@
+package com.example.chapter543;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter543Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter543Application.class, args);
+    }
+
+}
---
+++ chapter5-4-3/src/test/java/com/example/chapter543/Chapter543ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chapter543;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter543ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
